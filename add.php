d901c9214bba281cc3d70d241101f3acec6eb1f6
Zach Cutshall <97260107+zachcutshall@users.noreply.github.com>
2022-04-05 22:08:41 -0500

    Update add.php

--- add.php
+++ add.php
@@ -26,7 +26,7 @@ function test_input($data) {
 
 serverApi = new ServerApi(ServerApi::V1);
 $client = new MongoDB\Client(
-    'mongodb+srv://admin:specialwaddle@cluster0.twet7.mongodb.net/movieusers?retryWrites=true&w=majority', [], ['serverApi' => $serverApi]);
+    'mongodb+srv://admin:specialwaddle@cluster0.twet7.mongodb.net/movieuser?retryWrites=true&w=majority', [], ['serverApi' => $serverApi]);
 $user = $client->movieusers->user;
 
 $newuser = $user->insertOne(['name' => "$nameinput", 'movies' => ["$input1", "$input2", "$input3", "$input4", "$input5"]]);
