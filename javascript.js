5bc3adf959ab6db15810f8bb64ef0da58759af5a
Coplen <bcoplen@una.edu>
2022-04-08 15:59:52 -0500

    just minor changes left for the background

--- javascript.js
+++ javascript.js
@@ -5,31 +5,26 @@
 $(document).ready(function () {
 
 
-// randon image
-    const imgArr = ["KillBill.jpg","movieicon.jpg"];
+// Add image here (jpg only)
+    const imgArr = ["KillBill.jpg","JurassicPark.jpg","IT.jpg", "Guardians.jpg","Terminator.jpg","ET.jpg","Looper.jpg","Avatar.jpg","Jaws.jpg","Zombie.jpg","Pirates.jpg"];
     
 
-    // function randomnum(){
-    var num = Math.floor(Math.random() * (imgArr.length));
-    var l ="images/"+imgArr[num];
-    console.log(l);
-
-    // }
-
-  
-   
-  
     let posL = 400;
     let pstT = 100;
     
   
-    for (let i = 0; i < 88; i++) {
-      posL = Math.floor(Math.random() * 1000) + 1;
-      posT = Math.floor(Math.random() * 1000) + 1;
+    for (let i = 0; i < 100; i++) {
+      posL = Math.floor(Math.random() * 1500) + 1;
+      posT = Math.floor(Math.random() * 900) + 1;
 
-      var r = Math.random() * 50 - 5;
+      var r = Math.random() * 80 - 40;
       
-  
+      var num = Math.floor(Math.random() * (imgArr.length));
+      l = "images/"+imgArr[num];
+
+      
+
+    console.log(num, l);
       console.log(r);
   
       $("body").append(
